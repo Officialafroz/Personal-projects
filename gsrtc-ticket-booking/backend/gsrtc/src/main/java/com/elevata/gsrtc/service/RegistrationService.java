90e1cb2df52d10904bd2f6252796e9590a80eefd
Afroz Mansuri <lergapp@gmail.com>
2025-12-01 23:44:52 +0530

    add support user registration with otp

--- gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/service/RegistrationService.java
+++ gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/service/RegistrationService.java
@@ -1,4 +1,42 @@
 package com.elevata.gsrtc.service;
 
+import com.elevata.gsrtc.dto.EmailRequestDTO;
+import com.elevata.gsrtc.dto.OtpRequestDTO;
+import com.elevata.gsrtc.entity.RegistrationOtp;
+import com.elevata.gsrtc.repository.RegistrationOtpRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RegistrationService {
+    private RegistrationOtpRepository otpRepository;
+    private EmailService emailService;
+
+    @Autowired
+    public RegistrationService(RegistrationOtpRepository otpRepository, EmailService emailService) {
+        this.otpRepository = otpRepository;
+        this.emailService = emailService;
+    }
+
+    public void processRegistration(EmailRequestDTO emailRequestDTO) {
+        String email = emailRequestDTO.getEmail();
+        System.out.println(email);
+
+        int otp = OtpService.generateOtp();
+        RegistrationOtp registrationOtp = new RegistrationOtp(otp, email);
+
+        otpRepository.save(registrationOtp);
+        emailService.sendEmail(email,
+                "Registration code", "Your OTP code is " + otp);
+    }
+
+    public String verify(OtpRequestDTO otpRequest) {
+        String email = otpRequest.getEmail();
+        RegistrationOtp registrationOtp = otpRepository.findByEmail(otpRequest.getEmail());
+
+        if (registrationOtp != null) {
+            return "User verified";
+        }
+        return "Invalid email";
+    }
 }
