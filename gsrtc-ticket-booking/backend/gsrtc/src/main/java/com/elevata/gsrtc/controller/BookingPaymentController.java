0353027c22dfc0c7b357fd48e05f778f2e7e83ed
Afroz Mansuri <lergapp@gmail.com>
2025-12-10 19:20:01 +0530

    A API which saves payment data to DB. A special method which creates unique transaction reference no

--- gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/controller/BookingPaymentController.java
+++ gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/controller/BookingPaymentController.java
@@ -1,4 +1,24 @@
 package com.elevata.gsrtc.controller;
 
+import com.elevata.gsrtc.service.BookingPaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bookingPayment")
 public class BookingPaymentController {
+    private BookingPaymentService paymentService;
+
+    @Autowired
+    public BookingPaymentController(BookingPaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/pay")
+    public String add(@RequestParam double totalAmount, @RequestParam String pnr) {
+        return paymentService.save(totalAmount, pnr);
+    }
 }
