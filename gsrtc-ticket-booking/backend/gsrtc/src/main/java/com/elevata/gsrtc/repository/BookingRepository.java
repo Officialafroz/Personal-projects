05ce5f881d459b25357151c8d10319d731a262e4
Afroz Mansuri <lergapp@gmail.com>
2025-12-10 21:47:25 +0530

    added custom methods

--- gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/repository/BookingRepository.java
+++ gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/repository/BookingRepository.java
@@ -4,6 +4,11 @@ import com.elevata.gsrtc.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    Booking findByPnr(String pnr);
+    List<Booking> findAllByTripTripCode(String tripCode);
+    List<Booking> findAllByUserUid(Integer uid);
 }
