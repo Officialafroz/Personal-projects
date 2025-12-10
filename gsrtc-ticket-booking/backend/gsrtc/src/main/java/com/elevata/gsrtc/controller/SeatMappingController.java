d065117fb9d01afa88caba8c1fa9124c97972084
Afroz Mansuri <lergapp@gmail.com>
2025-12-10 19:04:56 +0530

    created an API to fetch booked seats from DB

--- gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/controller/SeatMappingController.java
+++ gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/controller/SeatMappingController.java
@@ -1,4 +1,27 @@
 package com.elevata.gsrtc.controller;
 
+import com.elevata.gsrtc.service.SeatMappingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/seats")
 public class SeatMappingController {
+    private SeatMappingService seatMappingService;
+
+    @Autowired
+    public SeatMappingController(SeatMappingService seatMappingService) {
+        this.seatMappingService = seatMappingService;
+    }
+
+    @GetMapping("/map")
+    public Set<Integer> mapSeats(@RequestParam String tripCode) {
+        return seatMappingService.mapSeats(tripCode);
+    }
+
 }
--- gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/service/SeatMappingService.java
+++ gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/service/SeatMappingService.java
@@ -1,4 +1,46 @@
 package com.elevata.gsrtc.service;
 
+import com.elevata.gsrtc.entity.Booking;
+import com.elevata.gsrtc.entity.Passenger;
+import com.elevata.gsrtc.repository.BookingRepository;
+import com.elevata.gsrtc.repository.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
 public class SeatMappingService {
+    private PassengerRepository passengerRepository;
+    private BookingRepository bookingRepository;
+
+    @Autowired
+    public SeatMappingService(PassengerRepository passengerRepository, BookingRepository bookingRepository) {
+        this.passengerRepository = passengerRepository;
+        this.bookingRepository = bookingRepository;
+    }
+
+
+    public Set<Integer> mapSeats(String tripCode) {
+        List<Booking> bookings = bookingRepository.findAllByTripTripCode(tripCode);
+        Set<Integer> seats = new HashSet<>();
+
+        if (bookings != null) {
+            for (Booking booking : bookings) {
+                List<Passenger> passengers = passengerRepository
+                        .findAllByBookingBookingId(booking.getBookingId());
+
+                for (Passenger passenger : passengers) {
+                    seats.add(passenger.getSeatNo());  // Set ignores duplicates automatically
+                }
+            }
+        }
+
+        System.out.println(bookings);
+        System.out.println(seats);
+
+        return seats;
+    }
 }
