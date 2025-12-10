99b9dc16a2c71c213f7717ee5f76482bdde2ba67
Afroz Mansuri <lergapp@gmail.com>
2025-12-10 22:02:25 +0530

    Added distance and duration support

--- gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/service/BusRouteService.java
+++ gsrtc-ticket-booking/backend/gsrtc/src/main/java/com/elevata/gsrtc/service/BusRouteService.java
@@ -24,18 +24,29 @@ public class BusRouteService {
     }
 
 
-    public void addRoute(RouteDTO routeDTO) {
+    public String addRoute(RouteDTO routeDTO, String duration) {
         Optional<BusDepot> busDepot = busDepotRepository.findById(routeDTO.getDepotId());
+        int durationInMinutes = convertToMinutes(duration);
 
         BusRoute route = new BusRoute();
         route.setRouteName(routeDTO.getRouteName());
         route.setStartPoint(routeDTO.getStartingPoint());
         route.setEndPoint(routeDTO.getEndingPoint());
         route.setClassType(routeDTO.getClassType());
+        route.setDistance(routeDTO.getDistance());
+        route.setDuration(durationInMinutes);
         busDepot.ifPresent(route::setBusDepot);
 
         routeRepository.save(route);
-        System.out.println("Route has been added to DB.");
+        return "Route has been added to DB.";
+    }
+
+    private int convertToMinutes(String duration) {
+        String[] parts = duration.split(":");
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+
+        return hours * 60 + minutes;
     }
 
     public List<RouteDTO> findAll() {
@@ -66,11 +77,13 @@ public class BusRouteService {
             routeDTO.setRouteName(route.getRouteName());
             routeDTO.setStartingPoint(route.getStartPoint());
             routeDTO.setEndingPoint(route.getEndPoint());
+            routeDTO.setClassType(route.getClassType());
+            routeDTO.setDistance(route.getDistance());
+            routeDTO.setDuration(route.getDuration());
             routeDTO.setDepotId(route.getBusDepot().getDepotId());
 
             routeDTOS.add(routeDTO);
         });
-
 
         return routeDTOS;
     }
